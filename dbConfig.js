73702e2c072a118b11bfc7e0dbf6f549e4092738
Humaira Muzammil <humaihr469@gmail.com>
2024-12-26 18:29:56 +0530

    configure DB

--- dbConfig.js
+++ dbConfig.js
@@ -8,7 +8,10 @@ dotenv.config();
 const isProduction = process.env.NODE_ENV ==="production";
 const connectionString =  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+      rejectUnauthorized: false, // Accepts self-signed certificates (typical for cloud services like Render)
+    },
 });
 
 export { pool };
