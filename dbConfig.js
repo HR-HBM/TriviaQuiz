9146dca6fd390e76f4ea784086b67844c6c18e25
Humaira Muzammil <humaihr469@gmail.com>
2025-02-04 08:56:24 +0530

    edit dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,26 +1,18 @@
 import dotenv from 'dotenv';
 import pkg from 'pg';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const { Pool } = pkg;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
+const { Pool } = pkg;
+
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const connectionString = process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?sslmode=require`;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction
-    ? {
-        rejectUnauthorized: false, // Ensure proper SSL verification
-        ca: process.env.CA_CERTIFICATE, // Use the environment variable for CA certificate
-      }
-    : false, // Disable SSL for local development
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Set to true if you can verify the cert chain properly
+  },
 });
 
 export { pool };
